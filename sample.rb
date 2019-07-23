c2bf8ad94768d8c8d0978cacfcfa904284df1cb9
Mediateri <ntawukuriryayomeddy1@gmail.com>
2019-07-23 09:30:41 +0200

    correcting

--- sample.rb
+++ sample.rb
@@ -38,14 +38,6 @@ end
 	 end
 
 
-	 def description(number)
-		player = Player.new(number)
-		arr=[0,1,2]
-		randomly=arr[rand(arr.count)];
-		enemy = Enemy.new(randomly)
-		janken = Janken.new(player,enemy)
-		puts " it is a #{janken.pon(player.hand, enemy.hand)}"
-	end
 while(true) do
 
 puts "please select one among these following:"
@@ -55,13 +47,21 @@ puts "2: Par"
 number=gets.to_i
 if(number<0 || number>2)
 	puts "out of boundary"
-elsif(number==2)
-	description(number)
-	break
-elsif(number==1)
-	description(number)
-	break
 else
-	description(number)
+	player = Player.new(number)
+	arr=[0,1,2]
+	randomly=arr[rand(arr.count)];
+	enemy = Enemy.new(randomly)
+	janken = Janken.new(player,enemy)
+	if(janken.pon(player.hand, enemy.hand)=="Win")
+		puts " it is a #{janken.pon(player.hand, enemy.hand)}"
+		break
+	elsif(janken.pon(player.hand, enemy.hand)=="Loss")
+		puts " it is a #{janken.pon(player.hand, enemy.hand)}"
+		break
+	else
+		puts " it is a #{janken.pon(player.hand, enemy.hand)}"
+end
+
 end
 end
