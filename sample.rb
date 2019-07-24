cf63a22f64ab0c1564c70e11aa0949df4260219e
Mediateri <ntawukuriryayomeddy1@gmail.com>
2019-07-24 08:31:03 +0200

    other correction

--- sample.rb
+++ sample.rb
@@ -6,30 +6,27 @@ end
 
 class Enemy
   def hand
-	arr=[0,1,2]
-	randomly=arr[rand(arr.count)];
-
-    return randomly
+	 arr=[0,1,2]
+	 randomly=arr[rand(arr.count)];
+	 return randomly
   end
 end
 	 
-	 class Janken
-		def pon(player_hand, enemy_hand)
-			result=((player_hand-enemy_hand+3)%3)
-			if(result==2)
-				return "Win"
-			elsif(result==1)
-				return "Loss"
-			else
-				return "Draw" 
-			end
-
+class Janken
+	def pon(player_hand, enemy_hand)
+		result=((player_hand-enemy_hand+3)%3)
+		if(result==2)
+			return "Win"
+		elsif(result==1)
+			return "Loss"
+		else
+			return "Draw" 
 		end
-	 end
+	end
+end
 
 
 while(true) do
-
 puts "please select one among these following:"
 puts "0: Goo"
 puts "1: Choki"
@@ -40,7 +37,6 @@ if(number<0 || number>2 )
 else
 	player = Player.new
 	pr=player.hand(number)
-	
 	enemy = Enemy.new
 	ene=enemy.hand
 	janken = Janken.new
